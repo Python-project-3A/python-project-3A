e513afaf952c46a7ab449171c152f846e237e6fc
memenikoi <emirrekiki308@gmail.com>
2025-11-03 16:26:03 +0100

    déplacé commentaires des méthodes pour suivre la syntaxe correcte

--- src/units/unit_base.py
+++ src/units/unit_base.py
@@ -10,7 +10,7 @@ class Unit :
         self.attack_range = attack_range
         self.attack_cooldown = attack_cooldown
         self.speed = speed
-        self.time_since_attack = 0.0
+        self.time_since_last_attack = 0.0
 
     def is_alive(self):
         """return True si l'unité est encore en vie """
@@ -27,40 +27,40 @@ class Unit :
         if not self.is_alive():
             pass
 
-    """
-    retourne un dictionnaire qui associe chaque nom d'attribut à sa valeur actuelle
-    peut être utile pour le save/load et la partie statistique plus tard
-    """
     def to_dict(self):
+        """
+        retourne un dictionnaire qui associe chaque nom d'attribut à sa valeur actuelle
+        peut être utile pour le save/load et la partie statistique plus tard
+        """
         return
 
-    """
-    calcule et retourne la distance entre deux unités
-    je pense que l'utilité est évidente
-    """
     def dist_to(self, other: "Unit") -> float:
+        """
+        calcule et retourne la distance entre deux unités
+        je pense que l'utilité est évidente
+        """
         return
 
-    """
-    un peu compliqué, déplace l'unité vers les coordonnées de l'unité cible
-    dépend de la vitesse de notre unité et du temps passé (dt)
-    dt: secondes par tick (vu que c'est pas un mouvement instantané)
-    retourne True si l'unité atteint la position cible (différence de 1 entre les x et y)
-    retourne False sinon
-    """
     def move_towards(self, target_x: int, target_y: int, dt: float) -> bool:
+        """
+        un peu compliqué, déplace l'unité vers les coordonnées de l'unité cible
+        dépend de la vitesse de notre unité et du temps passé (dt)
+        dt: secondes par tick (vu que c'est pas un mouvement instantané)
+        retourne True si l'unité atteint la position cible (diff de 1 entre les x et y)
+        retourne False sinon
+        """
         return
 
-    """
-    vérifie si les deux unités sont assez proches (donc dans l'attack range) pour attaquer
-    """
     def can_attack(self, other: "Unit") -> bool:
+        """
+        vérifie si les deux unités sont assez proches (selon attack range) pour attaquer
+        """
         return
 
-    """
-    attaquer une unité si on est assez proche
-    faut se rappeler qu'on un un délai entre les attaques (attack_cooldown)
-    modifie l'attribut time_since_last_attack
-    """
     def attack(self, other: "Unit"):
+        """
+        attaquer une unité si on est assez proche
+        faut se rappeler qu'on un un délai entre les attaques (attack_cooldown)
+        modifie l'attribut time_since_last_attack
+        """
         return
