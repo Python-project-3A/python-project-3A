49c39363fdeab1ada6561744594c09c5ddf931d9
memenikoi <emirrekiki308@gmail.com>
2025-11-03 15:07:32 +0100

    ajouté plus d'attributs à l'unité de base

--- src/units/unit_base.py
+++ src/units/unit_base.py
@@ -1,9 +1,13 @@
 class Unit : 
-    def __init__(self,name,hp,attack,armor,range,speed):
+    def __init__(self,name,team,x,y,hp,armor,damage,attack_range,attack_cooldown,speed):
         self.name = name 
+        self.team = team
+        self.x = x
+        self.y = y
         self.hp = hp
-        self.attack = attack
         self.armor = armor
-        self.range = range
+        self.damage = damage
+        self.attack_range = attack_range
+        self.attack_cooldown = attack_cooldown
+        self.attack_range = attack_range
         self.speed = speed
-        
