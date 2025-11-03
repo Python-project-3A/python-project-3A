7443895f3895e40048d6c9d9c2e8337c563840ea
manuvall2 <manu04fernando@gmail.com>
2025-11-03 14:56:06 +0100

    class unit de base

---
+++ src/units/unit_base.py
@@ -0,0 +1,9 @@
+class Unit : 
+    def __init__(self,name,hp,attack,armor,range,speed):
+        self.name = name 
+        self.hp = hp
+        self.attack = attack
+        self.armor = armor
+        self.range = range
+        self.speed = speed
+        
