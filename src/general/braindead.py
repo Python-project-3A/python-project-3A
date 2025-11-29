ae122db2b234a211c02b2f7e19e6840afe8885ce
Friedrich482 <friedricht109@gmail.com>
2025-11-29 16:56:37 +0100

    fix: optimize enemy detection in GeneralBraindead by using battlefield's enemy list

--- src/general/braindead.py
+++ src/general/braindead.py
@@ -27,25 +27,26 @@ class GeneralBraindead(BaseGeneral):
         No movement, no chasing, purely reactive.
         """
         my_units = self.get_my_units(battlefield)
+        enemies = self.get_enemy_units(battlefield)
+
+        if not enemies:
+            # No enemies left, we won!
+            return
 
         for unit in my_units:
             if not unit.is_alive():
                 continue
 
-            # Look for enemies ONLY within attack range (no chasing)
-            enemies_in_range = battlefield.units_in_radius(
-                unit.position[0],
-                unit.position[1],
-                unit.attack_range + 0.1,  # Slight buffer for floating point
-            )
-
-            # Filter to actual enemies
-            enemies_in_range = [e for e in enemies_in_range if e.owner != self.player_id and e.is_alive()]
+            # Find enemies within attack range (no chasing)
+            enemies_in_range = [
+                e for e in enemies 
+                if unit.can_attack(e)
+            ]
 
             if enemies_in_range:
                 # Enemy is right next to us - attack the nearest one
-                nearest = min(enemies_in_range, key=lambda e: unit.dist_to(e))
-                unit.current_order = {"type": "attack_unit", "target": nearest}
+                nearest_enemy = min(enemies_in_range, key=lambda e: unit.dist_to(e))
+                unit.current_order = {"type": "attack_unit", "target": nearest_enemy}
             else:
                 # No enemies nearby - do nothing (idle)
                 unit.current_order = None
