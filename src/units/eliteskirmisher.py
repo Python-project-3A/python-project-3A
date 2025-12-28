a79d00f454e05410c60fec2e754d235467efd46f
manuvall2 <manu04fernando@gmail.com>
2025-12-28 11:25:39 +0100

    units_avant_builds

---
+++ src/units/eliteskirmisher.py
@@ -0,0 +1,9 @@
+from .unit_base import Unit
+
+
+class EliteSkirmisher(Unit):
+    def __init__(self, owner: int, x: float, y: float, r: float, hp: int, armor: int, damage: int, attack_range: float, attack_cooldown: float, vision_range:float, speed: float, name: str = "EliteSkirmisher"):
+        super().__init__(name=name, owner=owner, x=x, y=y, r=r, hp=hp, armor=armor, damage=damage, attack_range=attack_range, attack_cooldown=attack_cooldown, vision_range=vision_range ,speed=speed)
+
+    def __repr__(self):
+        return f"<EliteSkirmisher id={self.id} owner={self.owner} pos={self.position} hp={self.hp}>"
