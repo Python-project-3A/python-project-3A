b743d8274d5a1e113a1a8caec5fc5f465e72b8c3
Shadow-0-5 <romain.lm05@gmail.com>
2026-01-22 12:19:21 +0100

    feat: attack improvement of pikemens

--- src/general/GeneralSmart.py
+++ src/general/GeneralSmart.py
@@ -159,7 +159,7 @@ class GeneralSmart(BaseGeneral):
 
     def _micro_pikeman_protector(self, unit: Unit, enemies: list["Unit"], protect_target_pos: tuple, default_target_pos: tuple, bf: Battlefield):
         """Logique : S'interposer entre la menace et les protégés. + Focus cavalerie"""
-        # 1. CIBLAGE
+        # 1. ANALYSE DE LA MENACE
         knights = self._filter_enemies(enemies, ["knight", "lightcavalry", "cavalryarcher"])
         threats = knights if knights else [e for e in enemies if e.is_alive()]
 
@@ -167,30 +167,36 @@ class GeneralSmart(BaseGeneral):
             unit.current_order = {"type": "attack_move", "target": default_target_pos}
             return
 
-        # 2. PROJECTION DU MOUVEMENT
+        nearest_threat = min(threats, key=lambda e: unit.dist_to(e))
+        is_cavalry_threat = nearest_threat.name.lower() in ["knight", "lightcavalry", "cavalryarcher"]
+
+        # --- BRANCHE 1 : Contre l'INFANTERIE (Daft Pikes, etc.) ---
+        if not is_cavalry_threat:
+            dist_to_nearest = unit.dist_to(nearest_threat)
+            if dist_to_nearest < unit.attack_range + 0.5:
+                enemies_in_range = [e for e in threats if unit.dist_to(e) <= unit.attack_range + 0.5]
+
+                if enemies_in_range:
+                    weakest_target = min(enemies_in_range, key=lambda e: e.hp)
+                    self._order_attack_opti(unit, weakest_target)
+                else:
+                    self._order_attack_opti(unit, nearest_threat)
+
+            else:
+                unit.current_order = {"type": "attack_move", "target": nearest_threat.position}
+
+            return
+        # --- BRANCHE 2 : Contre la CAVALERIE (Interception) ---
+
+        # 2. PROJECTION DU POINT D'ANCRAGE
         dir_x, dir_y = self.soustract_vec(default_target_pos, protect_target_pos)
         proj_ax, proj_ay = self._projection_vector(protect_target_pos, self.normalize_vec((dir_x, dir_y)), 10.0)  # On projette x mètres devant le groupe
 
-        # 3. INTERCEPTION DE LA MENACE
-        nearest_threat = min(threats, key=lambda e: (e.position[0] - proj_ax) ** 2 + (e.position[1] - proj_ay) ** 2)
-        is_fast_threat = nearest_threat.name.lower() in ["knight"]
-
-        # Calcul du point de blocage
+        # 3. CALCUL DU POINT DE BLOCAGE
         dx, dy = self.soustract_vec(nearest_threat.position, (proj_ax, proj_ay))
         dist_threat = (dx**2 + dy**2) ** 0.5
 
-        # --- LOGIQUE D'INTERCEPTION ---
-        if is_fast_threat:
-            # CAS 1 : CONTRE CAVALERIE
-            if dist_threat > 15.0:
-                ratio = 0.2
-            else:
-                ratio = 0.6  # On va chercher l'ennemi à 60% du chemin (Agressif)
-        else:
-            # CAS 2 : CONTRE INFANTERIE
-            ratio = 0.1  # On ne s'avance que de 10% vers l'ennemi (Défensif)
-
-        block_x, block_y = self._projection_vector((proj_ax, proj_ay), (dx, dy), ratio)
+        block_x, block_y = self._projection_vector((proj_ax, proj_ay), (dx, dy), 0.6)
 
         # --- ACTION ---
         if unit.dist_to(nearest_threat) < unit.attack_range + 0.5:
